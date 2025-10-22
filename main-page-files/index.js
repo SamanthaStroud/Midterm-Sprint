033589a2b97966152b09d92d694c591bf444a3e2
SamanthaStroud <samantha.stroud1@keyin.com>
2025-10-22 17:00:04 -0230

    mian page commit oct 22/25

---
+++ main-page-files/index.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementById("menubtn");
+CalButton.addEventListener("click", function () {
+  window.location.href = "menu-page-files/menupage.html";
+});
+
+//
+const orderButton = document.getElementById("orderbtn");
+GameButton.addEventListener("click", function () {
+  window.location.href = "ordering-page-files/orderpage.html";
+});
