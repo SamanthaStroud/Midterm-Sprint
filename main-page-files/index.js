973ec8bcaa5b932b2f5629f30b74fb9ef5b966e8
SamanthaStroud <samantha.stroud1@keyin.com>
2025-10-31 01:37:19 -0230

    Main page files Finished

--- main-page-files/index.js
+++ main-page-files/index.js
@@ -1,10 +1,11 @@
+//Menu Button
 const menuButton = document.getElementById("menubtn");
-CalButton.addEventListener("click", function () {
-  window.location.href = "menu-page-files/menupage.html";
+menuButton.addEventListener("click", function () {
+  window.location.href = "../menu-page-files/menupage.html";
 });
 
-//
-const orderButton = document.getElementById("orderbtn");
-GameButton.addEventListener("click", function () {
-  window.location.href = "ordering-page-files/orderpage.html";
+//Order Button
+const homeButton = document.getElementById("orderbtn");
+homeButton.addEventListener("click", function () {
+  window.location.href = "../ordering-page-files/orderpage.html";
 });
